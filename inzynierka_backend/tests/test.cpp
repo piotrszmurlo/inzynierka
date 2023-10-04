4c971215f7e2a6814f1feca087d7456878f3e4cc
Piotr Szmurlo <pszmurlo@box.com>
2023-10-04 17:55:07 +0200

    tests

--- inzynierka_backend/tests/test.cpp
+++ inzynierka_backend/tests/test.cpp
@@ -9,10 +9,30 @@
 #include <catch2/catch.hpp>
 #include "../src/funcs.cpp"
 
-TEST_CASE("median_calculate test", "[median]") {
-    std::vector<double> a;
-    a.push_back(1.3);
-    a.push_back(1.1);
-    a.push_back(1.5);
-    REQUIRE(median(a) == 1.3 );
+TEST_CASE("median_calculate uneven vector size", "[median]") {
+    std::vector<double> vector;
+    vector.push_back(1.3);
+    vector.push_back(1.1);
+    vector.push_back(1.5);
+    REQUIRE(median(vector) == 1.3 );
+}
+
+TEST_CASE("median_calculate even vector size", "[median]") {
+    std::vector<double> vector;
+    vector.push_back(1.3);
+    vector.push_back(1.1);
+    vector.push_back(1.5);
+    vector.push_back(1.6);
+    REQUIRE(median(vector) == 1.4 );
+}
+
+TEST_CASE("median_calculate one element vector", "[median]") {
+    std::vector<double> vector;
+    vector.push_back(1.1);
+    REQUIRE(median(vector) == 1.1 );
+}
+
+TEST_CASE("median_calculate empty vector", "[median]") {
+    std::vector<double> vector;
+    REQUIRE_THROWS_AS(median(vector), std::invalid_argument);
 }
